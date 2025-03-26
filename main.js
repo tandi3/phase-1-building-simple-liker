90bfcdb8086b79c245ddb577c04a758d7bbfc2ea
tandie oyucho <ysoyucho@gmail.com>
2025-03-26 12:23:30 +0300

    added changes

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+  const errorModal = document.getElementById("modal");
+  const hearts =document.querySelectorAll("like-glyph");
 
+  errorModal.classList.add("hidden");
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+      .then(() => {
+        if (heart.innerText === "♡") {
+          heart.innerText = "♥";
+          heart.classList.add("activated-heart");
+        } else {
+          heart.innerText = "♡";
+          heart.classList.remove("activated-heart");
+        }
+      })
+        .catch((error) => {
+          // Show error modal with message
+          errorModal.classList.remove("hidden");
+          errorModal.querySelector("#modal-message").innerText = error;
 
+          // Hide modal after 3 seconds
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
 
-
+    });
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
